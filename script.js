eabb1c09ada03dfcd95f088d2525e305cd456d0e
Anicakes1031 <153084248+Anicakes1031@users.noreply.github.com>
2024-12-09 12:12:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log ("Hello world");
+e = 85;
+m = 87;
+s = 95;
+f = 97;
+p = 60;
+
+a = ((e+m+s+f+p)/5);
+
+let result;
+ if (a>=75){
+    result ="chamba";
+} else{
+    result = "bobo";}
+alert ("English: "+ e +
+        "\nMath: " + m +
+        "\nScience: " + s +
+        "\nFilipino: " + f +
+        "\nPE: " + p  +
+        "\n\nAverage Grade: " + a + "\n" + result);
+
+confirm ("ok lang ba kapangitn ni she?");
+prompt ("anu masasabi sa kapangitan nila mamasang?");
